a3608068041770a11c1164388bf3281d53cf8417
Kieu Ngoc Trong <trong.kieu@infodation.local>
2020-11-03 15:12:45 +0700

    trong commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// The Date object will do what you want - construct one for each date, then compare them using the >, <, <= or >=.
+// The ==, !=, ===, and !== operators require you to use date.getTime()
 // var date1 = new Date();
 // var date2 = new Date("10/30/2020 17:34:24");
-// // compare 2 day với nhau
 // if (date1 > date2) {
 //     console.log('true');
 // } else {
 //     console.log('false');
 // }
+// var same = date1.getTime() === date2.getTime();
+// var notSame = date1.getTime() !== date2.getTime();
+
+
 
 
 
@@ -20,5 +25,15 @@
 // - day : numeric
 // - timeZone : UTC
 // - timeZoneName : long, short
+// const today = new Date();
+// console.log(`toLocaleString: ${today.toLocaleString('default', { month : 'short', day : 'numeric', year : 'numeric' })}`);
+
+
+
+
+
+
+
+//getTimezoneOffset() trả về khoảng thời gian khác biệt giữa múi giờ hiện tại của hệ thống và giờ UTC tính bằng phút.
 const today = new Date();
-console.log(`toLocaleString: ${today.toLocaleString('default', { month : 'short', day : 'numeric', year : 'numeric' })}`);
+console.log(`mui gio cua ban la ${ -(today.getTimezoneOffset() / 60)}`)
